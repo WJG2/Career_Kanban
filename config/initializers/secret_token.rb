9cb305fa47fcddbbd62ec63be0d3796011e73210
WJG2 <WJG2@gingles-air.fulcrum.net>
2013-09-27 15:07:04 -0700

    career kanban

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CareerKanban::Application.config.secret_key_base = '8ca123b2bbb51a9d1edbc4fee6af98985b92a13c4be3e9893d704a20ae80a6bcd8d8f7c46fc7001a4d7ee9bedb6e9a6302e159fc1a447efa3f0707ff88e82542'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CareerKanban::Application.config.session_store :cookie_store, key: '_career_kanban_session'
